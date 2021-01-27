83bf82f4abcf980f9457e7304bdeab757f831c31
mustbebuilt <martin@mustbebuilt.co.uk>
2021-01-27 15:34:22 +0000

    Tidied Logic

--- BasicFilmDbProject/Models/Film.cs
+++ BasicFilmDbProject/Models/Film.cs
@@ -12,13 +12,17 @@ namespace BasicFilmDbProject.Models
         public int FilmID { get; set; }
 
         [Required]
+        [Column(TypeName = "varchar(100)")]
         public string FilmTitle { get; set; }
 
         [Required]
+        [Column(TypeName = "varchar(5)")]
         public string FilmCertificate { get; set; }
 
+        [Column(TypeName = "text")]
         public string FilmDescription { get; set; }
 
+        [Column(TypeName = "varchar(100)")]
         public string FilmImage { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
