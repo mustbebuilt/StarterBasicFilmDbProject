df7e4f77c1d9eb82a248516d46e7f086299bd1e4
mustbebuilt <martin@mustbebuilt.co.uk>
2021-01-28 17:11:44 +0000

    Removed Finished Logic

--- BasicFilmDbProject/Controllers/HomeController.cs
+++ BasicFilmDbProject/Controllers/HomeController.cs
@@ -16,69 +16,17 @@ namespace BasicFilmDbProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _context = context;
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult AllMovies()
-        {
-            List<Film> model = _context.Films.ToList();
-            return View(model);
-        }
-
- public IActionResult Search(String SearchString, String certType)
-        {
-
-            var films = from m in _context.Films
-
-                        select m;
-
-            if (!string.IsNullOrEmpty(SearchString))
-
-            {
-
-                films = films.Where(s => s.FilmTitle.Contains(SearchString));
-
-            }
-
-            if (!string.IsNullOrEmpty(certType))
-            {
-                films = films.Where(x => x.FilmCertificate == certType);
-            }
-
-            var filmCerts = _context.Films.Select(m => m.FilmCertificate).Distinct();
-
-
-            List<Film> model = films.ToList();
-            ViewData["SearchString"] = SearchString;
-            ViewData["FilterFilmCert"] = certType;
-            ViewData["filmCerts"] = filmCerts.ToList();
-            ViewData["filmCertsSelectList"] = new SelectList(filmCerts.ToList());
-            return View(model);
-        }
-
-
-        // note use of the id from the routing pattern in startup.cs
-        [HttpGet]
-        public IActionResult FilmDetails(int id)
-        {
-            //List<Movie> model = _context.Movies.Find(FilmID);
-            Film model = _context.Films.Find(id);
-            return View(model);
-        }
-
-
-
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
